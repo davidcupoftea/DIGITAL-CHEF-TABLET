b8246a2eae427f905863d1f5287887f16600353e
David Ibáñez García <david@Davids-MacBook-Air.local>
2025-09-02 12:26:52 +0200

    Ahora hay tap and drag pero no ocurre nada más

--- src/components/MesaInCanva.jsx
+++ src/components/MesaInCanva.jsx
@@ -1,81 +1,37 @@
 import { useState } from "react";
+import { View } from "react-native";
 import Svg, { Rect, Circle } from "react-native-svg";
-import Draggable from "react-native-draggable";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-  Pressable,
-  TextInput,
-  Platform,
-  Alert,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
+import { PanGestureHandler, TapGestureHandler } from "react-native-gesture-handler";
 
 const MesaInCanva = ({ mesa, isEditing }) => {
   const [isRounded, setIsRounded] = useState(false);
   const [x, setX] = useState(mesa.x || 50);
   const [y, setY] = useState(mesa.y || 50);
 
-  const handleMesaPress = () => {
-    console.log("isEditing is", isEditing);
-    if (isEditing) {
-      console.log("isEditing is", isEditing);
-      setIsRounded(!isRounded); // Cambia forma en edición
-    }
+  const handleTap = () => {
+    console.log('taaaaap')
+    if (isEditing) setIsRounded(!isRounded);
   };
 
-  // Lo que dibuja la mesa
-  const mesaComponent = isRounded ? (
-    <Circle cx={25} cy={25} r={25} fill="orange" />
-  ) : (
-    <Rect x={0} y={0} width={50} height={50} fill="orange" />
-  );
+  const handleDrag = (event) => {
+    console.log('draaag')
+    console.log('event is', event)
+    setX(event.nativeEvent.translationX);
+    setY(event.nativeEvent.translationY);
+  };
 
-  return isEditing ? (
-    <Draggable
-    renderSize={50}
-      x={x}
-      y={y}
-      onDragRelease={(e, gestureState, bounds) => {
-        setX(bounds.left);
-        setY(bounds.top);
-      }}
-      onLongPress={() => console.log("long press")}
-      onShortPressRelease={() => console.log("press drag")}
-      onPressIn={() => console.log("in press")}
-      onPressOut={() => console.log("out press")} // <--- TOQUE CORTO
-    >
-      {/* <Pressable
-        style={{
-          width: 50,
-          height: 50,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        onPress={handleMesaPress} // Esto sí se dispara
-      >*/}
-      <Svg width={50} height={50}>
-        {mesaComponent}
-      </Svg>
-      {/* 
-      </Pressable> */}
-    </Draggable>
-  ) : // En modo normal, se pinta en el SVG padre
-  isRounded ? (
-    <Circle cx={x + 25} cy={y + 25} r={25} fill="orange" />
-  ) : (
-    <Rect
-      x={x}
-      y={y}
-      width={50}
-      height={50}
-      fill="orange"
-      onPress={handleMesaPress}
-    />
+  return (
+    <TapGestureHandler onActivated={handleTap}>
+      <PanGestureHandler onGestureEvent={handleDrag}>
+        <Svg x={x} y={y} width={50} height={50}>
+          {isRounded ? (
+            <Circle cx={25} cy={25} r={25} fill="orange" />
+          ) : (
+            <Rect x={0} y={0} width={50} height={50} fill="orange" />
+          )}
+        </Svg>
+      </PanGestureHandler>
+    </TapGestureHandler>
   );
 };
 
