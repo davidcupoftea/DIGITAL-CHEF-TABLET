70637ff179c24d7d7ab86c1819724d245def852e
David Ibáñez García <david@Davids-MacBook-Air.local>
2025-09-03 05:08:37 +0200

    Esto ya casi está

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -3,7 +3,18 @@ import Svg, { Rect, Circle } from "react-native-svg";
 import MesaInCanva from './MesaInCanva.jsx'
 
 const CanvasMesas = ({ mesas, isEditing }) => {
+const size = 50;
+const padding = 10;
+const mesasPorFila = 5; 
 
+  mesas.forEach((mesa, index) => {
+    if (mesa.x == null || mesa.y == null) {
+      const fila = Math.floor(index / mesasPorFila);
+      const col = index % mesasPorFila;
+      mesa.x = col * (size + padding);
+      mesa.y = fila * (size + padding);
+    }
+  });
 
   return (
     <View style={{ flex: 1, borderWidth: 1, borderColor: "gray" }}>
