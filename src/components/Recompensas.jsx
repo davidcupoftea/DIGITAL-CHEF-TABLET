c84d97c2d04dff886fb03c4ebb03c499ab0b8831
David Ibáñez García <david@Davids-MacBook-Air.local>
2025-09-17 18:32:57 +0200

    Restaurante o restaurantes

--- src/components/Recompensas.jsx
+++ src/components/Recompensas.jsx
@@ -100,7 +100,7 @@ function Recompensas() {
   return (
     <View style={styles.container}>
       <View>
-        <View><Text style={styles.pointstext}>Estás viendo las recompensas del restaurante {restaurantChosen.franchise}.</Text></View>
+        <View><Text style={styles.pointstext}>Estás viendo las recompensas del restaurante o restaurantes {restaurantChosen.franchise}.</Text></View>
         <Text style={styles.pointstext}>
           Invitar o ser invitado por un amigo otorga {pointsPerInvitation}{" "}
           puntos.
