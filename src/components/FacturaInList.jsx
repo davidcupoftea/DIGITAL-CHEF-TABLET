88ea3061ac77eae17ae7b2f8c0796c7b6e49661f
David Ibáñez García <david@Davids-MacBook-Air.local>
2025-09-18 23:39:14 +0200

    Añadido que se vea el precio del concepto en la representación de la factura in list

--- src/components/FacturaInList.jsx
+++ src/components/FacturaInList.jsx
@@ -1,17 +1,9 @@
 import { useState, memo, useContext, useRef } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Alert,
-} from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { AuthFlowContext } from "./AuthUseContextProvider.jsx";
 import { RestaurantChosenContext } from "./RestaurantChosenProvider.jsx";
-import {
-  BASE_URL,
-} from "../services/index.jsx";
+import { BASE_URL } from "../services/index.jsx";
 import { soloImprimirFacturaAsociada } from "../services/printerFunctions.jsx";
 import { PrinterContext } from "./PrintersContextProvider.jsx";
 
@@ -20,13 +12,13 @@ const NewsInList = ({ factura, fetchFacturas }) => {
   const [authTokens, setAuthTokens] = authTokensObject;
   let { restaurantChosenObject } = useContext(RestaurantChosenContext);
   const [restaurantChosen, setRestaurantChosen] = restaurantChosenObject;
-    const { selectedPrinters, setSelectedPrinters } = useContext(PrinterContext);
+  const { selectedPrinters, setSelectedPrinters } = useContext(PrinterContext);
 
-  const [anulada, setAnulada] = useState(factura.anulada)
+  const [anulada, setAnulada] = useState(factura.anulada);
 
   const navigation = useNavigation();
 
-    const askBeforeNullingAgain = (factura_id, fetchFacturas) => {
+  const askBeforeNullingAgain = (factura_id, fetchFacturas) => {
     Alert.alert(
       "DE NUEVO:¿Seguro que quieres anularlo?",
       "DE NUEVO:¿Estás seguro de que quieres anular esta factura? Esto sólo se debería hacer en ocasiones muy excepcionales",
@@ -43,7 +35,6 @@ const NewsInList = ({ factura, fetchFacturas }) => {
     );
   };
 
-
   const askBeforeNulling = (factura_id, fetchFacturas) => {
     Alert.alert(
       "¿Seguro que quieres anularlo?",
@@ -55,14 +46,15 @@ const NewsInList = ({ factura, fetchFacturas }) => {
         },
         {
           text: "Sí",
-          onPress: async () => await askBeforeNullingAgain(factura_id, fetchFacturas),
+          onPress: async () =>
+            await askBeforeNullingAgain(factura_id, fetchFacturas),
         },
       ]
     );
   };
 
   const nullFactura = async (factura_id, fetchFacturas) => {
-    Alert.alert('Anulando...', 'La factura seleccionada se está anulando')
+    Alert.alert("Anulando...", "La factura seleccionada se está anulando");
     const res = await fetch(
       BASE_URL + "null-invoice/" + restaurantChosen.pk + "/",
       {
@@ -76,120 +68,209 @@ const NewsInList = ({ factura, fetchFacturas }) => {
     );
 
     var jsonData2 = await res.json();
-    if (jsonData2.status == 'ok'){
-      Alert.alert('Factura anulada con éxito', jsonData2.message)
-      setAnulada(true)
+    if (jsonData2.status == "ok") {
+      Alert.alert("Factura anulada con éxito", jsonData2.message);
+      setAnulada(true);
       fetchFacturas(restaurantChosen.pk);
-    } else if (jsonData2.status == 'nook'){
-      Alert.alert('Problema', jsonData2.message)
+    } else if (jsonData2.status == "nook") {
+      Alert.alert("Problema", jsonData2.message);
       fetchFacturas(restaurantChosen.pk);
     }
-    
   };
-
 
   return (
     <>
       <View style={styles.card}>
         <View style={styles.insidecard}>
-          {anulada?(<View>
-            <Text style={styles.textBoldError}>ANULADA</Text>
-          </View>):null}
+          {anulada ? (
+            <View>
+              <Text style={styles.textBoldError}>ANULADA</Text>
+            </View>
+          ) : null}
           {factura.needed_to_resend ? (
             <View>
               <Text style={styles.textBoldError}>SE NECESITA REENVIAR</Text>
             </View>
           ) : null}
           <View>
-            <Text style={styles.text}>Identificador:{'\n'}{factura.ticket_identifier}</Text>
+            <Text style={styles.text}>
+              Identificador:{"\n"}
+              {factura.ticket_identifier}
+            </Text>
           </View>
           <View>
-            <Text style={styles.text}>Creado en:{'\n'}{factura.created}</Text>
+            <Text style={styles.text}>
+              Creado en:{"\n"}
+              {factura.created}
+            </Text>
           </View>
           <View>
-            <Text style={styles.text}>Restaurante:{'\n'}{factura.restaurant.franchise}, en {factura.restaurant.address}, {factura.restaurant.localidad}</Text>
+            <Text style={styles.text}>
+              Restaurante:{"\n"}
+              {factura.restaurant.franchise}, en {factura.restaurant.address},{" "}
+              {factura.restaurant.localidad}
+            </Text>
           </View>
           <View>
-            <Text style={styles.text}>Tipo de factura:{'\n'}{factura.ticket_identifier_serie}</Text>
+            <Text style={styles.text}>
+              Tipo de factura:{"\n"}
+              {factura.ticket_identifier_serie}
+            </Text>
           </View>
           <View>
-            <Text style={factura.negative?styles.textredcentered:styles.text}>Negativa (abono):{'\n'}{factura.negative.toString()}</Text>
+            <Text
+              style={factura.negative ? styles.textredcentered : styles.text}
+            >
+              Negativa (abono):{"\n"}
+              {factura.negative.toString()}
+            </Text>
           </View>
           <View>
-            {factura.ticket_identifier_serie == 'F1'?<Text style={styles.text}>Nif destinatario:{'\n'}{factura.nif.toString()}</Text>:null}
+            {factura.ticket_identifier_serie == "F1" ? (
+              <Text style={styles.text}>
+                Nif destinatario:{"\n"}
+                {factura.nif.toString()}
+              </Text>
+            ) : null}
           </View>
           <View>
-            {factura.ticket_identifier_serie == 'F1'?<Text style={styles.text}>Domicilio fiscal destinatario:{'\n'}{factura.domicilio_fiscal}</Text>:null}
+            {factura.ticket_identifier_serie == "F1" ? (
+              <Text style={styles.text}>
+                Domicilio fiscal destinatario:{"\n"}
+                {factura.domicilio_fiscal}
+              </Text>
+            ) : null}
           </View>
           <View>
-            {factura.ticket_identifier_serie == 'F1'?<Text style={styles.text}>Nombre destinatario:{'\n'}{factura.nombre_o_razon_social}</Text>:null}
+            {factura.ticket_identifier_serie == "F1" ? (
+              <Text style={styles.text}>
+                Nombre destinatario:{"\n"}
+                {factura.nombre_o_razon_social}
+              </Text>
+            ) : null}
           </View>
           <View>
-            <Text style={styles.text}>Rectificativa por diferencias:{'\n'}{factura.rectificativa_por_diferencias.toString()}</Text>
+            <Text style={styles.text}>
+              Rectificativa por diferencias:{"\n"}
+              {factura.rectificativa_por_diferencias.toString()}
+            </Text>
           </View>
           <View>
-            <Text style={styles.text}>Rectificativa sustitutiva:{'\n'}{factura.rectificativa_sustitutiva.toString()}</Text>
+            <Text style={styles.text}>
+              Rectificativa sustitutiva:{"\n"}
+              {factura.rectificativa_sustitutiva.toString()}
+            </Text>
           </View>
           <View>
-            <Text style={styles.text}>Comentario rectificacion:{'\n'}{factura.comentario_rectificacion}</Text>
+            <Text style={styles.text}>
+              Comentario rectificacion:{"\n"}
+              {factura.comentario_rectificacion}
+            </Text>
           </View>
           <View>
-            <Text style={styles.text}>Rectificada:{'\n'}{factura.rectificada?.ticket_identifier}</Text>
+            <Text style={styles.text}>
+              Rectificada:{"\n"}
+              {factura.rectificada?.ticket_identifier}
+            </Text>
           </View>
           <View>
-            {factura.ticket_identifier_serie == 'F1'?<Text style={styles.text}>Ordinaria de simplificada:{'\n'}{factura.ordinaria_de_simplificada.toString()}</Text>:null}
+            {factura.ticket_identifier_serie == "F1" ? (
+              <Text style={styles.text}>
+                Ordinaria de simplificada:{"\n"}
+                {factura.ordinaria_de_simplificada.toString()}
+              </Text>
+            ) : null}
           </View>
-          <View><Text style={styles.text}>Pedido:</Text>
-            {factura.order_elements.length > 0 ? factura.order_elements.map((element, index)=>(
-              <Text style={factura.negative? styles.textred :null} key={index}>{element.dish} (x{element.quantity}) [ID:{element.dish_pk}] - {element.new_price_corrected?element.new_price_corrected:element.dish_price} €</Text>
-            )): <Text style={factura.negative? styles.textred :null} >No hay platos</Text>}
+          <View>
+            <Text style={styles.text}>Pedido:</Text>
+            {factura.order_elements.length > 0 ? (
+              factura.order_elements.map((element, index) => (
+                <Text
+                  style={factura.negative ? styles.textred : null}
+                  key={index}
+                >
+                  {element.dish} (x{element.quantity}) [ID:{element.dish_pk}] -{" "}
+                  {element.new_price_corrected
+                    ? element.new_price_corrected
+                    : element.dish_price}{" "}
+                  €
+                </Text>
+              ))
+            ) : (
+              <Text style={factura.negative ? styles.textred : null}>
+                No hay platos
+              </Text>
+            )}
           </View>
-          <View><Text style={styles.text}>Conceptos:</Text>
-            {factura.conceptos_extra.length > 0 ? factura.conceptos_extra.map((element, index)=>(
-              <Text style={factura.negative? styles.textred :null} key={index}>{element.description}</Text>
-            )): <Text style={factura.negative? styles.textred :null}>No hay conceptos</Text>}
+          <View>
+            <Text style={styles.text}>Conceptos:</Text>
+            {factura.conceptos_extra.length > 0 ? (
+              factura.conceptos_extra.map((element, index) => (
+                <Text
+                  style={factura.negative ? styles.textred : null}
+                  key={index}
+                >
+                  {element.description} - [{element.price} €]
+                </Text>
+              ))
+            ) : (
+              <Text style={factura.negative ? styles.textred : null}>
+                No hay conceptos
+              </Text>
+            )}
           </View>
 
-          <Text style={styles.text}>Facturado por:{" "}{factura.created_by}</Text>
+          <Text style={styles.text}>Facturado por: {factura.created_by}</Text>
 
-          {factura.available_from_simplified_to_ordinary ?
-          (<TouchableOpacity
-            style={styles.buttonyellow}
-            onPress={() => { 
-              navigation.navigate('De simplificada a ordinaria', {eventId: factura.id})
-            }}
-          >
-            <Text style={styles.textbuttonblack}>Convertir en ordinaria</Text>
-          </TouchableOpacity>)
-          :
-          null}
+          {factura.available_from_simplified_to_ordinary ? (
+            <TouchableOpacity
+              style={styles.buttonyellow}
+              onPress={() => {
+                navigation.navigate("De simplificada a ordinaria", {
+                  eventId: factura.id,
+                });
+              }}
+            >
+              <Text style={styles.textbuttonblack}>Convertir en ordinaria</Text>
+            </TouchableOpacity>
+          ) : null}
 
           <TouchableOpacity
             style={styles.buttongreen}
-            onPress={() => { 
-              navigation.navigate('Factura Rectificativa', {eventId: factura.id})
+            onPress={() => {
+              navigation.navigate("Factura Rectificativa", {
+                eventId: factura.id,
+              });
             }}
           >
             <Text style={styles.textbutton}>Rectificar</Text>
           </TouchableOpacity>
 
-
-          {!anulada?<TouchableOpacity
-            style={styles.buttonred}
-            onPress={() => {
-              askBeforeNulling(factura.id, fetchFacturas);
-            }}
-          >
-            <Text style={styles.textbutton}>Anular</Text>
-          </TouchableOpacity>:null}
+          {!anulada ? (
+            <TouchableOpacity
+              style={styles.buttonred}
+              onPress={() => {
+                askBeforeNulling(factura.id, fetchFacturas);
+              }}
+            >
+              <Text style={styles.textbutton}>Anular</Text>
+            </TouchableOpacity>
+          ) : null}
 
           <TouchableOpacity
             style={styles.buttonblue}
             onPress={() => {
-              soloImprimirFacturaAsociada(factura.id, restaurantChosen, authTokens?.access, selectedPrinters)
+              soloImprimirFacturaAsociada(
+                factura.id,
+                restaurantChosen,
+                authTokens?.access,
+                selectedPrinters
+              );
             }}
           >
-            <Text style={styles.textbutton}>Imprimir última factura asociada</Text>
+            <Text style={styles.textbutton}>
+              Imprimir última factura asociada
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -207,7 +288,7 @@ const styles = StyleSheet.create({
     borderWidth: 4,
     borderColor: "white",
     borderRadius: 30,
-    marginHorizontal:15,
+    marginHorizontal: 15,
   },
   text: {
     color: "black",
@@ -223,7 +304,7 @@ const styles = StyleSheet.create({
     fontFamily: "Function-Regular",
     marginBottom: 10,
   },
-    textBoldError: {
+  textBoldError: {
     color: "red",
     fontSize: 30,
     textAlign: "center",
@@ -233,9 +314,8 @@ const styles = StyleSheet.create({
   insidecard: {
     padding: 15,
   },
-  textred:{
-    color: 'red',
-
+  textred: {
+    color: "red",
   },
   buttonred: {
     marginTop: 10,
@@ -267,7 +347,7 @@ const styles = StyleSheet.create({
     shadowColor: "black",
     shadowOpacity: 1,
   },
-    buttonyellow: {
+  buttonyellow: {
     marginTop: 10,
     padding: 6,
     backgroundColor: "yellow",
@@ -282,7 +362,7 @@ const styles = StyleSheet.create({
     shadowColor: "black",
     shadowOpacity: 1,
   },
-    buttonblue: {
+  buttonblue: {
     marginTop: 10,
     padding: 6,
     backgroundColor: "blue",
@@ -303,12 +383,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontFamily: "Function-Regular",
   },
-    textbuttonblack: {
+  textbuttonblack: {
     fontSize: 25,
     color: "black",
     textAlign: "center",
     fontFamily: "Function-Regular",
-  }
+  },
 });
 
 export default memo(NewsInList);
